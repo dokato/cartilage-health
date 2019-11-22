6f70d284f2c40978ef1b474af6d0ee9d7666296a
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2019-11-22 16:55:19 +0000

    wrapped into function, added cli and way to store data as json files; saving plots (?) for later

--- projection.py
+++ projection.py
@@ -1,10 +1,10 @@
+import sys
 import math
+import json
 import numpy as np 
 import matplotlib.pyplot as plt
 from scipy import optimize
 from PIL import Image
-
-fig = False
 
 def calc_R(x,y, xc, yc):
     "distance of each 2D points from the center (xc, yc)"
@@ -41,6 +41,10 @@ def pol2cart(rho, phi):
     return(x, y)
 
 def get_region_value(regiondict, binned_super, binned_deep, angle, slice):
+    """
+    Thresholding data from *binned_super*, *binned_deep* and updating
+    *regiondict* dictionary.
+    """
     if angle <=-35:
         if slice <= 40:
             regiondict['SLA'].extend(list(binned_super[:,2]))
@@ -63,101 +67,129 @@ def get_region_value(regiondict, binned_super, binned_deep, angle, slice):
             regiondict['SUP'].extend(list(binned_super[:,2]))
             regiondict['DUP'].extend(list(binned_deep[:,2]))
 
-masked = np.load('9003126_t2.npy') 
-thickness_div = 0.5
+def projection(masked_cartilage, thickness_div = 0.5, values_threshold = 100,
+                     angular_bin = 5, fig = False):
+    '''
+    This fitting femoral cartilage to a cylinder, then binning every *angular_bin*
+    degrees all pixels.
 
-nr_slices = masked.shape[0]
+    IN:
+        masked_cartilage - matrix with masked cartilage T2 values
+        thickness_div - a proportion of cartilage one considers 'deep' (default: 0.5)
+        values_threshold - threshold to eliminate unreasonably high pixels (default: 100)
+        angular_bin - how densly sample the cylinder fit for region averaging (default: 5)
+        fig - logical value, whether to plot figures or not (default: False)
+    OUT:
+        dictionary with fields encoding averaged values of cartilage T2 map
+    '''
+    masked = masked_cartilage
+    nr_slices = masked.shape[0]
 
-masked[masked > 100] = 0
-mask_proj = np.max(masked, axis = 0)
+    masked[masked > values_threshold] = 0
+    mask_proj = np.max(masked, axis = 0)
 
-x_coord, y_coord = np.nonzero(mask_proj)
+    x_coord, y_coord = np.nonzero(mask_proj)
+    xc, yc, R, _ = leastsq_circle(x_coord, y_coord)
 
-xc, yc, R, _ = leastsq_circle(x_coord, y_coord)
+    if fig:
+        plt.imshow(mask_proj)
+        theta_fit = np.linspace(-np.pi, np.pi, 180)
+        x_fit = xc + R * np.cos(theta_fit)
+        y_fit = yc + R * np.sin(theta_fit)
+        plt.plot(x_fit, y_fit, 'b-' , label="fitted circle", lw=2)
+        plt.plot([xc], [yc], 'bD', mec='y', mew=1)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.plot(x_coord, y_coord, 'g.', label='data', mew=1)
+        plt.show()
 
-if fig:
-    plt.imshow(mask_proj)
-    theta_fit = np.linspace(-np.pi, np.pi, 180)
-    x_fit = xc + R * np.cos(theta_fit)
-    y_fit = yc + R * np.sin(theta_fit)
-    plt.plot(x_fit, y_fit, 'b-' , label="fitted circle", lw=2)
-    plt.plot([xc], [yc], 'bD', mec='y', mew=1)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.plot(x_coord, y_coord, 'g.', label='data', mew=1)
-    plt.show()
+    visualization       = np.zeros((nr_slices,72))
+    super_visualization = np.zeros((nr_slices,72))
+    deep_visualization  = np.zeros((nr_slices,72))
+    superficial_values  = []
+    deep_values         = []
+    regions = ['SLA','SLC','SLP','SUA','SUC','SUP','DLA','DLC','DLP','DUA','DUC', 'DUP']
+    regiondict = dict((i, []) for i in regions)
 
-visualization       = np.zeros((nr_slices,72))
-super_visualization = np.zeros((nr_slices,72))
-deep_visualization  = np.zeros((nr_slices,72))
-superficial_values  = []
-deep_values         = []
-regions = ['SLA','SLC','SLP','SUA','SUC','SUP','DLA','DLC','DLP','DUA','DUC', 'DUP']
-regiondict = dict((i, []) for i in regions)
-
-for i in range(nr_slices):
-    if np.all(masked[i, :, :]==0):
-        continue
-    x_inds, y_inds = np.nonzero(np.squeeze(masked[i,:,:]))
-    x_ind_c, y_ind_c = x_inds - xc, y_inds - yc
-    values = masked[i, x_inds, y_inds] 
-
-    rho, theta = cart2pol(x_ind_c, y_ind_c)
-    theta = np.array(list(map(math.degrees, theta))) # transf. to degrees
-    print(i, np.min(theta), np.max(theta))
-    polar_matrix = np.vstack((theta, rho, values)).T
-
-    angles = np.arange(-180,180,5)
-    for e, angle in enumerate(angles):
-        bottom_bin, top_bin = angle, angle + 5
-        splice_matrix = np.logical_and(theta > bottom_bin, theta <= top_bin)
-        if np.all(splice_matrix == False):
+    for i in range(nr_slices):
+        if np.all(masked[i, :, :]==0):
             continue
-        if i==12 and angle==-90:
-            import pdb;pdb.set_trace()
-        polar_in_cone = polar_matrix[splice_matrix]
-        max_radius = np.max(polar_in_cone[:,1])
-        min_radius = np.min(polar_in_cone[:,1])
+        x_inds, y_inds = np.nonzero(np.squeeze(masked[i,:,:]))
+        x_ind_c, y_ind_c = x_inds - xc, y_inds - yc
+        values = masked[i, x_inds, y_inds] 
 
-        cart_thickness = max_radius - min_radius
-        rad_division = min_radius + cart_thickness * thickness_div
+        rho, theta = cart2pol(x_ind_c, y_ind_c)
+        theta = np.array(list(map(math.degrees, theta))) # transf. to degrees
+        polar_matrix = np.vstack((theta, rho, values)).T
 
-        splice_deep = polar_in_cone[:,1] <= rad_division
-        binned_deep  = polar_in_cone[splice_deep]
-        binned_super = polar_in_cone[~splice_deep]
+        angles = np.arange(-180, 180, angular_bin)
+        for e, angle in enumerate(angles):
+            bottom_bin, top_bin = angle, angle + angular_bin
+            splice_matrix = np.logical_and(theta > bottom_bin, theta <= top_bin)
+            if np.all(splice_matrix == False):
+                continue
+            polar_in_cone = polar_matrix[splice_matrix]
+            max_radius = np.max(polar_in_cone[:,1])
+            min_radius = np.min(polar_in_cone[:,1])
 
-        superficial_values.extend(list(binned_super[:,2]))
-        deep_values.extend(list(binned_deep[:,2]))
-        get_region_value(regiondict, binned_super, binned_deep, angle, i)
-        visualization[i, e] = np.mean(polar_in_cone[:,2])
-        super_visualization[i, e] = np.mean(binned_super[:,2])
-        deep_visualization[i, e] = np.mean(binned_deep[:,2])
+            cart_thickness = max_radius - min_radius
+            rad_division = min_radius + cart_thickness * thickness_div
 
+            splice_deep = polar_in_cone[:,1] <= rad_division
+            binned_deep  = polar_in_cone[splice_deep]
+            binned_super = polar_in_cone[~splice_deep]
 
-averagedT2 = masked.sum()/np.count_nonzero(masked)
+            superficial_values.extend(list(binned_super[:,2]))
+            deep_values.extend(list(binned_deep[:,2]))
+            get_region_value(regiondict, binned_super, binned_deep, angle, i)
+            visualization[i, e] = np.mean(polar_in_cone[:,2])
+            super_visualization[i, e] = np.mean(binned_super[:,2])
+            deep_visualization[i, e] = np.mean(binned_deep[:,2])
 
-print(averagedT2)
-print(np.mean(superficial_values))
-print(np.mean(deep_values))
-for k in regiondict:
-    print(k, np.mean(regiondict[k]))
+    averagedT2 = masked.sum()/np.count_nonzero(masked)
+    avg_vals_dict = {}
+    avg_vals_dict['all'] = averagedT2
+    print('All : {:.2f}'.format(averagedT2))
+    avg_vals_dict['superficial'] = np.mean(superficial_values)
+    print('Super : {:.2f}'.format(np.mean(superficial_values)))
+    avg_vals_dict['deep'] = np.mean(deep_values)
+    print('Deep : {:.2f}'.format(np.mean(deep_values)))
+    print('-'*5)
+    for k in regiondict:
+        avg_vals_dict[k] = np.mean(regiondict[k])
+        print('{} : {:.2f}'.format(k, np.mean(regiondict[k])))
 
-if fig:
-    plt.subplot(2,2,1)
-    im1 = Image.fromarray(visualization)                                    
-    im2 = im1.resize((512,512))                                           
-    plt.imshow(im2)
-    plt.colorbar()
-    plt.title('All')
-    plt.subplot(2,2,2)
-    im1 = Image.fromarray(super_visualization)                                    
-    im2 = im1.resize((512,512))                                           
-    plt.imshow(im2)
-    plt.colorbar()
-    plt.title('superficial')
-    plt.subplot(2,2,3)
-    im1 = Image.fromarray(deep_visualization)                                    
-    im2 = im1.resize((512,512))                                           
-    plt.imshow(im2)
-    plt.colorbar()
-    plt.title('deep')
+    if fig:
+        plt.subplot(2,2,1)
+        im1 = Image.fromarray(visualization)                                    
+        im2 = im1.resize((512,512))                                           
+        plt.imshow(im2)
+        plt.colorbar()
+        plt.title('All')
+        plt.subplot(2,2,2)
+        im1 = Image.fromarray(super_visualization)                                    
+        im2 = im1.resize((512,512))                                           
+        plt.imshow(im2)
+        plt.colorbar()
+        plt.title('superficial')
+        plt.subplot(2,2,3)
+        im1 = Image.fromarray(deep_visualization)                                    
+        im2 = im1.resize((512,512))                                           
+        plt.imshow(im2)
+        plt.colorbar()
+        plt.title('deep')
+        plt.show()
+
+    return avg_vals_dict
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        raise IOError("Expected name of the 'npy' file with masked T2 values")
+    file_name = sys.argv[1]
+    if file_name == 'x':
+        file_name = '9003126_t2.npy'
+    masked = np.load(file_name)
+    avg_vals_dict = projection(masked)
+    save_file_name = file_name.split('.')[0] + '_avg_vals.json'
+    with open(save_file_name, "w") as f:
+        f.write(json.dumps(avg_vals_dict))
